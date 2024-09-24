6a18e342e5acf20d865c57c8862308ec8428f736
Bappy60 <kawsar.ahmed.bappy.148@gmail.com>
2024-09-24 12:58:55 +0600

    added two gestureDetectorgender button

--- lib/main.dart
+++ lib/main.dart
@@ -12,26 +12,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: const Color(0xFF0A0E21),
+        scaffoldBackgroundColor: const Color(0xFF0A0E21),
       ),
       home: const MyHomePage(title: 'BMI Calculator'),
+      // darkTheme: ThemeData.dark(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -40,41 +32,127 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void _onPressSelected() {
+    print("Selected");
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+      appBar: _MyAppBar(title: widget.title),
+      body: Column(
+        children: [
+          _buildGenderSelector(),
+          // _buildGHeightSlider(),
+          // _buildWeightSetter(),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const Icon(Icons.icecream_sharp),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Widget _buildGenderSelector() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onTap: _onPressSelected,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF111328),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.male,
+                          size: 80.0,
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Text(
+                          "Male",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Color(0xFFFFFFFF),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: GestureDetector(
+                  onTap: _onPressSelected,
+                  child: Container(
+                    // margin: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF111328),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.female,
+                          size: 80.0,
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Text(
+                          "Female",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Color(0xFFFFFFFF),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildGHeightSlider() {
+    return Container();
+  }
+
+  Widget _buildWeightSetter() {
+    return const Placeholder();
+  }
+}
+
+class _MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String _title;
+
+  const _MyAppBar({
+    required String title,
+  }) : _title = title;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      title: Text(_title),
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => const Size.square(30);
 }
